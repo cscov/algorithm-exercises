1ce6a0483c51811639f142fc6fbc6e7dd455b37c
cscov <carolyn.l.scoville@gmail.com>
2018-04-30 23:36:11 -0700

    pushes and pops

--- dynamicArrays/Carolyn_Scoville_Algorithms1/lib/ring_buffer.rb
+++ dynamicArrays/Carolyn_Scoville_Algorithms1/lib/ring_buffer.rb
@@ -7,25 +7,41 @@ class RingBuffer
     @length = 0
     @capacity = 8
     @store = StaticArray.new(capacity)
+    @start_idx = 0
   end
 
   # O(1)
   def [](index)
     check_index(index)
+
     store[index]
   end
 
   # O(1)
   def []=(index, val)
+    check_index(index)
+
     store[index] = val
   end
 
   # O(1)
   def pop
+    popped = store[length - 1]
+
+    store[length - 1] = nil
+    self.length -= 1
+
+    popped
   end
 
   # O(1) ammortized
   def push(val)
+    resize! if length == capacity
+
+    store[length] = val
+    self.length += 1
+
+    store
   end
 
   # O(1)
