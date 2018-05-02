150b18b41bc620db97711479f70940e4ece4a7a8
cscov <carolyn.l.scoville@gmail.com>
2018-05-01 17:24:00 -0700

    Finshed MaxIntSet

--- LRU_Cache/Carolyn_Scoville_Algorithms2/lib/p01_int_set.rb
+++ LRU_Cache/Carolyn_Scoville_Algorithms2/lib/p01_int_set.rb
@@ -1,22 +1,32 @@
 class MaxIntSet
   def initialize(max)
+    @max_int = max
+    @store = Array.new(max, false)
+
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true unless self.include?(num)
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false unless !self.include?(num)
   end
 
   def include?(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    num >= 0 && num <= @max_int
   end
 
   def validate!(num)
+    raise 'Out of bounds' unless is_valid?(num)
   end
 end
 
@@ -27,18 +37,21 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
