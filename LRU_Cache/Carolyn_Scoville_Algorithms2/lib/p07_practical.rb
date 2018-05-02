56c5bfdc8f85dfd963a9b8a5ec71063edfc9b40f
cscov <carolyn.l.scoville@gmail.com>
2018-05-02 11:56:15 -0700

    Partially completed palindrome exercise

--- LRU_Cache/Carolyn_Scoville_Algorithms2/lib/p07_practical.rb
+++ LRU_Cache/Carolyn_Scoville_Algorithms2/lib/p07_practical.rb
@@ -1,5 +1,6 @@
 require_relative 'p05_hash_map'
-
-def can_string_be_palindrome?(string) 
-  
+# count
+def can_string_be_palindrome?(string)
+  map_hash = HashMap.new(string.length)
+  string.each_char.with_index { |chr, idx| map_hash.set(idx, chr) }
 end
