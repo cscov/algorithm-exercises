dbd1bd7489d449f22f1177a342bc20cf3262ac07
cscov <carolyn.l.scoville@gmail.com>
2018-05-01 17:42:11 -0700

    Finished ResizingIntSet

--- LRU_Cache/Carolyn_Scoville_Algorithms2/lib/p01_int_set.rb
+++ LRU_Cache/Carolyn_Scoville_Algorithms2/lib/p01_int_set.rb
@@ -69,18 +69,26 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if count == num_buckets
+
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -88,5 +96,14 @@ class ResizingIntSet
   end
 
   def resize!
+    new_num_buckets = num_buckets * 2
+    new_store = Array.new(new_num_buckets) { Array.new }
+
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_store[el % new_num_buckets] << el
+      end
+    end
+    @store = new_store
   end
 end
