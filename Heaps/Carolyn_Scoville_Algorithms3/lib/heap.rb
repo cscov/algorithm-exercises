65b75a364c9720e41a9474e960fa7c332f8b647d
cscov <carolyn.l.scoville@gmail.com>
2018-05-02 23:45:34 -0700

    Heapify down correctly

--- Heaps/Carolyn_Scoville_Algorithms3/lib/heap.rb
+++ Heaps/Carolyn_Scoville_Algorithms3/lib/heap.rb
@@ -16,17 +16,25 @@ class BinaryMinHeap
   end
 
   def extract
+    root = self.peek
+    @store.last, store.first = store.first, store.last
+    @store.pop
+
+    BinaryMinHeap.heapify_up(@store, count - 1)
+    root
   end
 
   def peek
+    @store[0]
   end
 
   def push(val)
+    @store.push(val)
+    BinaryMinHeap.heapify_up(@store, count - 1) if count > 1
   end
 
   public
   def self.child_indices(len, parent_index)
-    # debugger
     current = parent_index
     child_indices = []
     child_indices << (2 * current) + 1 if (2 * current) + 1 < len
@@ -40,8 +48,25 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    # debugger
+    children = BinaryMinHeap.child_indices(len, parent_idx)
+    if !children.empty?
+      lesser = children[0]
+      lesser = children[1] if children[1] && array[children[1]] < array[children[0]]
+      while array[lesser] < array[parent_idx]
+        array[lesser], array[parent_idx] = array[parent_idx], array[lesser]
+        BinaryMinHeap.heapify_down(array, lesser)
+      end
+    end
+    array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    parent_idx = BinaryMinHeap.parent_index(child_idx)
+    parent = array[parent_idx]
+    while parent > array[child_idx]
+      array[child_idx], array[parent_idx] = array[parent_idx],
+                                                      array[child_idx]
+    end
   end
 end
