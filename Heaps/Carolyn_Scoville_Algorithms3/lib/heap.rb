145eb33b7e1a5fdb746d57c1bf959b9840014a4b
cscov <carolyn.l.scoville@gmail.com>
2018-05-02 22:37:57 -0700

    child indices working

--- Heaps/Carolyn_Scoville_Algorithms3/lib/heap.rb
+++ Heaps/Carolyn_Scoville_Algorithms3/lib/heap.rb
@@ -1,10 +1,18 @@
+require 'byebug'
 class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
+    @store = []
+    if prc.nil?
+      @prc = Proc.new { |el1, el2| -1 * (el1 <=> el2) }
+    else
+      @prc = prc
+    end
   end
 
   def count
+    @store.length
   end
 
   def extract
@@ -18,6 +26,12 @@ class BinaryMinHeap
 
   public
   def self.child_indices(len, parent_index)
+    # debugger
+    current = parent_index
+    child_indices = []
+    child_indices << (2 * current) + 1 if (2 * current) + 1 < len
+    child_indices << (2 * current) + 2 if (2 * current) + 2 < len
+    child_indices
   end
 
   def self.parent_index(child_index)
