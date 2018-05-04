e08b485d08844f9ac25f0f9a0cbf0e855ea54f06
cscov <carolyn.l.scoville@gmail.com>
2018-05-04 10:34:11 -0700

    Partition is working without proc

--- QuickSort/Carolyn_Scoville_Algorithms4/lib/quick_sort.rb
+++ QuickSort/Carolyn_Scoville_Algorithms4/lib/quick_sort.rb
@@ -1,9 +1,15 @@
+require 'byebug'
 class QuickSort
   # Quick sort has average case time complexity O(nlogn), but worst
   # case O(n**2).
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    pivot_idx = QuickSort.partition(array, 0, array.length)
+    left = array[start...pivot_idx]
+    right = array[pivot_idx + 1...array.length]
+
+
   end
 
   # In-place.
@@ -11,5 +17,25 @@ class QuickSort
   end
 
   def self.partition(array, start, length, &prc)
+    # debugger
+
+    pivot = array[start]
+    pivot_idx = array.index(pivot)
+    boundary = start
+    finish_idx = start + length - 1
+
+    while boundary != finish_idx
+      check_num = array[boundary + 1]
+      if check_num < pivot
+        array[pivot_idx], array[boundary + 1] = array[boundary + 1], array[pivot_idx]
+        pivot_idx = array.index(pivot)
+        boundary += 1
+      else
+        array[boundary + 1], array[finish_idx] = array[finish_idx], array[boundary + 1]
+        finish_idx -= 1
+      end
+    end
+
+    array.index(pivot)
   end
 end
