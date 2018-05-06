425c16586e8d5bb1bd2b78b4ec28ba9b3e2096a7
cscov <carolyn.l.scoville@gmail.com>
2018-05-06 11:41:31 -0700

    Added parent attribute to node, started insert

--- BST/Carolyn_Scoville_Algorithms5/lib/binary_search_tree.rb
+++ BST/Carolyn_Scoville_Algorithms5/lib/binary_search_tree.rb
@@ -1,11 +1,27 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
 
 class BinarySearchTree
+  attr_reader :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if root == nil
+      root.value = value
+      return
+    end
+    parent = traverse(root, value)
+    if compare_values(parent.value, value) <= 0
+      parent.left = BSTNode.new(value)
+      node = parent.left
+    else
+      parent.right = BSTNode.new(value)
+      node = parent.right
+    end
+    node.parent = parent
   end
 
   def find(value, tree_node = @root)
@@ -19,7 +35,7 @@ class BinarySearchTree
   end
 
   def depth(tree_node = @root)
-  end 
+  end
 
   def is_balanced?(tree_node = @root)
   end
@@ -30,5 +46,31 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+  def check_current_node_value(node)
+    node.value
+  end
 
+  def compare_values(val1, val2)
+    val1 <=> val2
+  end
+
+  def has_left_child(node)
+    node.left.nil?
+  end
+
+  def has_right_child(node)
+    node.right.nil?
+  end
+
+  def traverse(node, value)
+    if check_current_node_value(node).nil?
+      nil
+    elsif compare_values(node.value, value) <= 0 && node.has_left_child
+      traverse(node.left, value)
+    elsif compare_values(node.value, value) == 1 && node.has_left_child
+      traverse(node.right, value)
+    else
+      node
+    end
+  end
 end
--- BST/Carolyn_Scoville_Algorithms5/lib/bst_node.rb
+++ BST/Carolyn_Scoville_Algorithms5/lib/bst_node.rb
@@ -1,9 +1,10 @@
 class BSTNode
   attr_reader :value
-  attr_accessor :left, :right
+  attr_accessor :left, :right, :parent
   def initialize(value)
     @value = value
     @right = nil
     @left = nil
+    @parent = nil
   end
 end
