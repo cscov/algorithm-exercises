2020fb4a75406e0c46bf66f226a8eb7d287f7c56
cscov <carolyn.l.scoville@gmail.com>
2018-05-07 10:00:26 -0700

    Created second draft of kth_largest value

--- BST/Carolyn_Scoville_Algorithms5/lib/bst_practical.rb
+++ BST/Carolyn_Scoville_Algorithms5/lib/bst_practical.rb
@@ -1,6 +1,25 @@
 require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
-  values = tree_node.in_order_traversal
-  values[-1 - k]
+  visited_nodes = []
+  left_path = false
+  right_path = false
+
+  while visited_nodes.length < k
+    visited_nodes << tree_node
+    if tree_node.left
+      visited_nodes << tree_node.left
+      left_path = true
+    end
+    if tree_node.right
+      visited_nodes << tree_node.right
+      right_path = true
+    end
+    if left_path
+      tree_node = tree_node.left
+    elsif right_path
+      tree_node = tree_node.right
+    end
+    visited_nodes.last
+  end
 end
